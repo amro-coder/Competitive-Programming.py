fef5a962faf8634ccd238bdee545006acf4bfa92
amro-coder <AmroAdil420@gmail.com>
2022-01-06 13:23:02 +0000

    third commit

--- CommonLounge/DaynmicProgramming/Tiling problem.py
+++ CommonLounge/DaynmicProgramming/Tiling problem.py
@@ -1,1 +1,13 @@
-print("to be solved")
+n=int(input())
+# recursive approach
+# def rec(n):
+#     if(n<=1):
+#         return 1
+#     return rec(n-1)+rec(n-2)
+# print(rec(n))
+# now lets use DP
+#base case
+dp=[1,1]
+for i in range(2,n+1):
+    dp.append(dp[i-1]+dp[i-2])
+print(dp[n])

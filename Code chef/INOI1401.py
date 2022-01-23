6a33f65d15dec9c6966f574862ecdef3fd8ba941
amro-coder <AmroAdil420@gmail.com>
2022-01-23 14:27:09 +0000

    DP problem

--- Code chef/INOI1401.py
+++ Code chef/INOI1401.py
@@ -12,15 +12,12 @@ for _ in range(r):
     grid.append(tuple(map(int,input().split())))
 dp=[[[[0]*(d+1),[0]*(d+1)] for __ in range(c+1)] for _ in range(r+1)]
 for i in range(d+1):
-    dp[-2][-2][0][i]=dp[-2][-2][1][i]=1
+    dp[-2][-2][0][i]=1
 for i in range(r-1,-1,-1):
     for j in range(c-1,-1,-1):
         if(i==r-1 and j==c-1) or grid[i][j]==0:
             continue
         for moves in range(1,d+1):
-            # dir =0 means right
-            dp[i][j][0][moves]=dp[i][j+1][0][moves-1]+dp[i+1][j][1][d-1]
-            dp[i][j][0][0]=dp[i+1][j][1][d-1]
-            dp[i][j][1][moves]=dp[i][j+1][0][d-1]+dp[i+1][j][1][moves-1]
-            dp[i][j][1][0]=dp[i][j+1][0][d-1]
-print(max(dp[0][0][0][d],dp[0][0][1][d])%20011)
+            dp[i][j][0][moves]=(dp[i][j+1][0][moves-1]%20011+dp[i][j+1][1][d]%20011)%20011
+            dp[i][j][1][moves]=(dp[i+1][j][0][d]%20011+dp[i+1][j][1][moves-1]%20011)%20011
+print((dp[0][0][0][d]+dp[0][0][1][d])%20011)

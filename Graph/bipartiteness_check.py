bd8fa4915c64ced0d142a227fb7f6dd4214b261f
amro-coder <AmroAdil420@gmail.com>
2022-02-27 23:02:29 +0000

    self rebalancig tree, insert and delete takes log(n) , and order of elements is maintained

--- Graph/bipartiteness_check.py
+++ Graph/bipartiteness_check.py
@@ -1,36 +1,46 @@
 from collections import deque
-def BFS_colouring(start):
-    q=deque([start])
-    visited[start]=True
-    while q:
-        parent=q.popleft()
+def is_bipartite():
+    visited = [False] * n
+    black = set()
+    white = set()
+    def BFS_colouring(start):
+        q = deque([start])
+        visited[start] = True
+        black.add(start)
+        while q:
+            parent = q.popleft()
+            for child in graph[parent]:
+                if (not visited[child]):
+                    visited[child] = True
+                    q.append(child)
+                    if (parent in black):
+                        white.add(child)
+                    else:
+                        black.add(child)
+    for i in range(n):
+        if not visited[i]:
+            BFS_colouring(i)
+    for parent in black:
         for child in graph[parent]:
-            if (not visited[child]):
-                visited[child]=True
-                q.append(child)
-                colours[child]=not colours[parent]
-    return
+            if child in black:
+                return False,set(),set()
+    for parent in white:
+        for child in graph[parent]:
+            if child in white:
+                return False,set(),set()
+    return True,black,white
+
 n,m=map(int,input().split())
-graph=[[] for _ in range(n+1)]
+graph=[[] for _ in range(n)]
 for _ in range(m):
     a,b=map(int,input().split())
     graph[a].append(b)
     graph[b].append(a)
-visited=[False]*(n+1)
-colours=[False]*(n+1)
-for i in range(n+1):
-    if(not visited[i]):
-        BFS_colouring(i)
-ans=True
-for parent in range(n+1):
-    for child in graph[parent]:
-        if(not(colours[parent]^colours[child])):
-            ans=False
-            break
-if(ans):
-    print("graph is bipartite")
-else:
-    print("graph is not bipartite")
+
+ans=is_bipartite()
+print("graph is not bipartite" if not ans[0] else f"blacks are {ans[1]} and whites are {ans[2]}")
+
+
 # input example
 # 10 11
 # 0 1
@@ -44,3 +54,5 @@ else:
 # 8 9
 # 9 10
 # 4 1
+
+
